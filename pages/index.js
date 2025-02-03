6f5a67a274e274bb4ff4a21b6e974d180bed4b55
goncy <gonzalo.pozzo4@gmail.com>
2025-02-03 08:49:16 -0300

    Remove swr header

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export const getServerSideProps = context => {
   context.res.setHeader('Vary', 'Cookie');
 
   // Set the Cache-Control header to cache the response for 10 seconds
-  context.res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate=59');
+  context.res.setHeader('Cache-Control', 's-maxage=10');
 
 
   // Get the timestamp to ensure the response is cached
